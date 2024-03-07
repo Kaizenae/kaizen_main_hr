04c801427d50ace0e6e1f59d8db787edfebb5b7e
Kaizenae <A.Saed@kaizenae.com>
2024-03-06 22:14:08 -0800

    Edit version to 3.1.1

--- lib/main.dart
+++ lib/main.dart
@@ -81,7 +81,7 @@ class MyApp extends StatelessWidget {
             data: MediaQuery.of(context)
                 .copyWith(textScaler: const TextScaler.linear(1.0)),
             child: Material(child: child!)),
-        title: 'KAIZEN HR',
+        title: 'IDC HR',
         theme: getApplicationTheme(),
         routes: RoutesMap.routesMap(),
 
